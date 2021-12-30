f564dd3c0c45f5535a7990ff9dfe1f1b2212626a
Robert Godlewski <robtechg5@gmail.com>
2021-12-30 08:37:03 -0800

    added in step 2.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 # 1. TASK: print "Hello World"
 print('Hello World')
 # 2. print "Hello Noelle!" with the name in a variable
-# name = "Noelle"
-# print( your code here )	# with a comma
-# print( your code here )	# with a +
+name = 'Robert'
+print('Hello', name)	# with a comma
+a = 'Hello '
+print(a + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
 # name = 42
 # print( your code here )	# with a comma
