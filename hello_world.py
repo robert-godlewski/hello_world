93cb8b481bd603edb5bd7862c2d060a1086f7daf
Robert Godlewski <robtechg5@gmail.com>
2021-12-30 08:51:22 -0800

    added in step 4.

--- hello_world.py
+++ hello_world.py
@@ -10,7 +10,8 @@ name = 5
 print(a, name)	# with a comma
 print(a + ' ' + str(name))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
-# fave_food1 = "sushi"
-# fave_food2 = "pizza"
-# print( your code here ) # with .format()
-# print( your code here ) # with an f string
+fave_food1 = "eggs"
+fave_food2 = "pretzels"
+print('I love to eat ' + fave_food1 + ' and ' + fave_food2 + '.') # with string catenation
+print('I love to eat {} and {}.'.format(fave_food1, fave_food2)) # with .format()
+print(f'I love to eat {fave_food1} and {fave_food2}.') # with an f string
