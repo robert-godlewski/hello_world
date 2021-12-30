5c2ad859158503db619bc64403acd204ef6c1930
Robert Godlewski <robtechg5@gmail.com>
2021-12-30 08:43:13 -0800

    added in step 3.

--- hello_world.py
+++ hello_world.py
@@ -3,12 +3,12 @@ print('Hello World')
 # 2. print "Hello Noelle!" with the name in a variable
 name = 'Robert'
 print('Hello', name)	# with a comma
-a = 'Hello '
-print(a + name)	# with a +
+a = 'Hello'
+print(a + ' ' + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
-# name = 42
-# print( your code here )	# with a comma
-# print( your code here )	# with a +	-- this one should give us an error!
+name = 5
+print(a, name)	# with a comma
+print(a + ' ' + str(name))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 # fave_food1 = "sushi"
 # fave_food2 = "pizza"
